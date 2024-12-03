16eb659426f52b8319fea97b39c41cb464d76bdf
Daniil Slizov <slizovdaniil29@gmail.com>
2024-12-03 17:32:59 +0300

    На ветку develop

--- app/src/main/java/com/example/myprofile/MainActivity.java
+++ app/src/main/java/com/example/myprofile/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myprofile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,36 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView helloText;
+    private ImageButton startButton;
+    private Button developerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        developerButton = findViewById(R.id.buttonDeveloper); // Инициализация кнопки разработчика
+
+        // Установка обработчика нажатия для кнопки "О разработчике"
+        developerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class); // Создание интента
+                startActivity(intent); // Запуск новой активности
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void onStartButtonClick(View view) {
+        helloText.setVisibility(View.VISIBLE);
+        startButton.setClickable(false);
+    }
 }
+
